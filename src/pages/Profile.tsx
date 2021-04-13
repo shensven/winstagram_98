e1f87f01efa25453dbf0f7021ad17310329b4e33
shensven <shensven2016@gmail.com>
2021-04-13 23:30:55 +0800

    Profile: work in progress

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,22 +1,31 @@
 import React from 'react';
 import {WebView} from 'react-native-webview';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 // import 'xp.css/dist/98.css';
 
 const Profile: React.FC = () => {
     return (
-        <View style={style.root}>
+        <View style={styles.root}>
             <WebView
                 originWhitelist={['*']}
                 source={{html: '<button>Hello world</button>'}}
             />
+            <TouchableOpacity style={styles.button}>
+                <Text>Hello World</Text>
+            </TouchableOpacity>
         </View>
     );
 };
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
     root: {
         flex: 1,
+    },
+    button: {
+        alignSelf: 'center',
+        width: 100,
+        padding: 10,
+        margin: 10,
     },
 });
 
