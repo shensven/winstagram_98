d10d255802307ff46d9129591006c3f7dc81eaa6
shensven <shensven2016@gmail.com>
2021-04-17 16:21:27 +0800

    App.tsx: work in progress

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const Hello = () => {
-  return <div>Hello World</div>;
+  return <div>Hello World 2</div>;
 };
 
 const App = () => {
