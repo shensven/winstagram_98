79527cb5fc6b75a785c880d6eb8757ba5316e1c4
shensven <shensven2016@gmail.com>
2021-05-07 00:46:45 +0800

    Profile: refactoring the UI of existing

--- src/pages/Profile.module.scss.d.ts
+++ src/pages/Profile.module.scss.d.ts
@@ -1,42 +1,28 @@
 declare namespace ProfileModuleScssNamespace {
   export interface IProfileModuleScss {
-    bg: string;
-    edit_profile_btn: string;
-    edit_profile_wing: string;
-    first_margin_top: string;
     grid: string;
-    header: string;
     header_avatar: string;
-    header_avatar_img: string;
     header_base_info: string;
     header_bio: string;
-    header_divider: string;
+    header_btn: string;
     header_name: string;
     header_stats: string;
-    header_stats_folowers: string;
-    header_stats_folowers_num: string;
-    header_stats_folowers_title: string;
-    header_stats_folowing: string;
-    header_stats_folowing_num: string;
-    header_stats_folowing_title: string;
-    header_stats_post: string;
-    header_stats_post_num: string;
-    header_stats_post_title: string;
-    header_username: string;
+    header_stats_followering: string;
+    header_stats_followers: string;
+    header_stats_followers_num: string;
+    header_stats_followers_title: string;
+    header_stats_following_num: string;
+    header_stats_following_title: string;
+    header_stats_posts: string;
+    header_stats_posts_num: string;
+    header_stats_posts_title: string;
     header_website: string;
-    header_wing: string;
     img: string;
-    init_btn: string;
-    init_btn_ctrl: string;
-    mix_navgrid: string;
+    profile_info: string;
+    root: string;
     tab_item: string;
     tabs: string;
-    titlebar: string;
-    titlebar_btn: string;
-    titlebar_svg: string;
-    titlebar_title: string;
-    titlebar_title_logo: string;
-    titlebar_title_typo: string;
+    username: string;
   }
 }
 
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,98 +1,67 @@
 import React from 'react';
 import Nav from './Nav';
 import styles from './Profile.module.scss';
-import title_help from './icons/help.svg';
-import logo from './icons/glyph-logo_May2016.svg';
 import avatar from './images/avatar.jpg';
 import Button from './component/Button';
+import Window from './component/Window';
+import Divider from './component/Divider';
 
 const Profile: React.FC = () => {
   return (
-    <div className={styles.bg}>
-      <div className={styles.first_margin_top} />
-
-      {/* title bar */}
-      <div className={styles.titlebar}>
-        <div className={styles.titlebar_title}>
-          <img alt="" src={logo} className={styles.titlebar_title_logo} />
-          <span className={styles.titlebar_title_typo}>Winstagram 98</span>
-        </div>
-        <button type="button" className={styles.titlebar_btn}>
-          <img alt="" src={title_help} className={styles.titlebar_svg} />
-        </button>
-      </div>
-
-      {/* header */}
-      <div className={styles.header}>
-        <div className={styles.header_username}>shensven</div>
-        <div className={styles.header_divider} />
-        <div className={styles.header_wing}>
-          <div className={styles.header_base_info}>
-            <div className={styles.header_avatar}>
-              <img alt="" src={avatar} className={styles.header_avatar_img} />
+    <Window className={styles.root}>
+      <div className={styles.username}>shensven</div>
+      <Divider />
+      <div className={styles.header_base_info}>
+        <img alt="" src={avatar} className={styles.header_avatar} />
+        <div>
+          <div className={styles.header_stats}>
+            <div className={styles.header_stats_posts}>
+              <div className={styles.header_stats_posts_num}>334</div>
+              <div className={styles.header_stats_posts_title}>posts</div>
             </div>
-            <div>
-              <div className={styles.header_stats}>
-                <span className={styles.header_stats_post}>
-                  <div className={styles.header_stats_post_num}>334</div>
-                  <div className={styles.header_stats_post_title}> posts</div>
-                </span>
-                <span className={styles.header_stats_folowers}>
-                  <div className={styles.header_stats_folowers_num}>221K</div>
-                  <div className={styles.header_stats_folowers_title}>
-                    folowers
-                  </div>
-                </span>
-                <span className={styles.header_stats_folowing}>
-                  <div className={styles.header_stats_folowing_num}>134</div>
-                  <div className={styles.header_stats_folowing_title}>
-                    foloweing
-                  </div>
-                </span>
+            <div className={styles.header_stats_followers}>
+              <div className={styles.header_stats_followers_num}>221K</div>
+              <div className={styles.header_stats_followers_title}>
+                followers
               </div>
-              <div className={styles.edit_profile_wing}>
-                <Button className={styles.edit_profile_btn}>
-                  Edit Profile
-                </Button>
+            </div>
+            <div className={styles.header_stats_followering}>
+              <div className={styles.header_stats_following_num}>134</div>
+              <div className={styles.header_stats_following_title}>
+                following
               </div>
             </div>
           </div>
-          <div className={styles.header_name}>GenSven</div>
-          <div className={styles.header_bio}>Frontend Developer</div>
-          <div className={styles.header_website}>blog.shensven.com</div>
+          <Button className={styles.header_btn}>Edit Profile</Button>
         </div>
-        <div className={styles.header_divider} />
       </div>
-
-      {/* tab */}
+      <div className={styles.profile_info}>
+        <div className={styles.header_name}>GenSven</div>
+        <div className={styles.header_bio}>Frontend Developer</div>
+        <div className={styles.header_website}>blog.shensven.com</div>
+      </div>
+      <Divider />
       <div className={styles.tabs}>
         <Button className={styles.tab_item}>Grid</Button>
         <Button className={styles.tab_item}>List</Button>
         <Button className={styles.tab_item}>Lacation</Button>
         <Button className={styles.tab_item}>Save</Button>
       </div>
-
-      {/* nav & grid */}
-      <div className={styles.mix_navgrid}>
-        {/* grid */}
-        <div className={styles.grid}>
-          <div className={styles.img}>1</div>
-          <div className={styles.img}>2</div>
-          <div className={styles.img}>3</div>
-          <div className={styles.img}>4</div>
-          <div className={styles.img}>5</div>
-          <div className={styles.img}>6</div>
-          <div className={styles.img}>7</div>
-          <div className={styles.img}>8</div>
-          <div className={styles.img}>9</div>
-          <div className={styles.img}>10</div>
-          <div className={styles.img}>11</div>
-        </div>
-
-        {/* nav */}
-        <Nav />
+      <div className={styles.grid}>
+        <div className={styles.img}>1</div>
+        <div className={styles.img}>2</div>
+        <div className={styles.img}>3</div>
+        <div className={styles.img}>4</div>
+        <div className={styles.img}>5</div>
+        <div className={styles.img}>6</div>
+        <div className={styles.img}>7</div>
+        <div className={styles.img}>8</div>
+        <div className={styles.img}>9</div>
+        <div className={styles.img}>10</div>
+        <div className={styles.img}>11</div>
       </div>
-    </div>
+      <Nav />
+    </Window>
   );
 };
 
