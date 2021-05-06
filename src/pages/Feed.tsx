7a98257f1f9ca91d3dac6acd60d2505af0180304
shensven <shensven2016@gmail.com>
2021-05-07 01:11:27 +0800

    Feed: bug fixes & performance improvements

--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { useHistory } from 'react-router';
 import Window from './component/Window';
 import Nav from './Nav';
 // import styles from './Feed.module.scss';
 
 const Feed: React.FC = () => {
-  const history = useHistory();
   return (
     <div>
       <Window>
