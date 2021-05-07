86ff275e9de2f647cbb01bf33bc15ad6fad90ce3
shensven <shensven2016@gmail.com>
2021-05-07 20:44:03 +0800

    Profile: bug fixes & performance improvements

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useLocation } from 'react-router';
 import Nav from './Nav';
 import styles from './Profile.module.scss';
