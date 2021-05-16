7cfb08b722896f2d60e53561d1e8ef75fb422862
shensven <shensven2016@gmail.com>
2021-05-17 00:24:06 +0800

    Containers: create some placeholder on <Grid />

--- src/pages/containers/Grid.tsx
+++ src/pages/containers/Grid.tsx
@@ -3,19 +3,14 @@ import styles from './Grid.module.scss';
 
 const Grid = (props: any) => {
   const { className } = props;
+  const feedArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
   return (
     <div className={`${styles.grid} ${className}`}>
-      <div className={styles.img}>1</div>
-      <div className={styles.img}>2</div>
-      <div className={styles.img}>3</div>
-      <div className={styles.img}>4</div>
-      <div className={styles.img}>5</div>
-      <div className={styles.img}>6</div>
-      <div className={styles.img}>7</div>
-      <div className={styles.img}>8</div>
-      <div className={styles.img}>9</div>
-      <div className={styles.img}>10</div>
-      <div className={styles.img}>11</div>
+      {feedArr.map((index: number, item: any) => (
+        <div className={styles.img} key={index}>
+          {feedArr[item]}
+        </div>
+      ))}
     </div>
   );
 };
