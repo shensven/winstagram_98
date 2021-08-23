5433942d5ad599ed4a1bab3fb8aedf6562141556
shensven <shensven2016@gmail.com>
2021-08-23 10:31:54 +0800

    chore: update electron-react-boilerplate to 3.0.2

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,6 @@ module.exports = {
   rules: {
     // A temporary hack related to IDE not resolving correct package.json
     'import/no-extraneous-dependencies': 'off',
-    // 'prettier/prettier': ['warn', { tabWidth: 4 }],
-    // 'prettier.eslintIntegration': false,
   },
   parserOptions: {
     ecmaVersion: 2020,
---
+++ src/renderer/pages/components/Window/Window.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import icon from '../../icons/glyph-logo_May2016.svg';
+import title_help from '../../icons/help.svg';
+import styles from './Window.module.scss';
+
+const Window = (props: any) => {
+  const { children, className } = props;
+  const [hasTitlebar] = useState(false);
+
+  return (
+    <div className={styles.bg}>
+      <div className={styles.first_margin_top} />
+      <div
+        className={`
+          ${styles.titlebar}
+          ${hasTitlebar ? null : styles.titlebar_display}
+        `}
+      >
+        <div className={styles.titlebar_title}>
+          <img alt="" src={icon} className={styles.titlebar_title_icon} />
+          <span className={styles.titlebar_title_typo}>Winstagram 98</span>
+        </div>
+        <button type="button" className={styles.titlebar_btn}>
+          <img alt="" src={title_help} className={styles.titlebar_svg} />
+        </button>
+      </div>
+      <div className={`${styles.children} ${className}`}>{children}</div>
+    </div>
+  );
+};
+
+export default Window;
